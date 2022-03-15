9ae54734108d62aa93c97d563172d1cde70fbb51
ilyait <ilya_2.0@mail.ru>
2022-03-15 04:12:39 +0300

    Create connect with subd MySQL via JDBC

---
+++ JavaCourseWork/src/main/DB_Connect.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.SQLOutput;
+
+public class DB_Connect {
+    public static Connection getConnection() {
+        Connection connection = null;
+   
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/CourseWork", "username", "password");
+
+            if (connection != null) {
+                System.out.println("Connection ok!");
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+};
