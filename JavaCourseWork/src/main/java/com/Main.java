27fb24df1a5680e01a55cb97e355533f109eaa45
ilyait <ilya_2.0@mail.ru>
2022-04-12 02:48:11 +0300

    Using JPA to connect to DB

--- JavaCourseWork/src/main/java/com/Main.java
+++ JavaCourseWork/src/main/java/com/Main.java
@@ -1,14 +1,9 @@
 package main.java.com;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class Main {
     public static void main(String[] args) {
-        Connection connection = DB_Connect.getConnection();
         SpringApplication.run(Main.class, args);
     }
 }
