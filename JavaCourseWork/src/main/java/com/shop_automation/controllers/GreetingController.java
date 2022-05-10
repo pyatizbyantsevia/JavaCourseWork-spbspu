ab24de0723d4a34a852a4b37f351ad140002f7b1
iLyxaN1ce <ilya_2.0@mail.ru>
2022-05-10 05:51:04 +0300

    ui changes

--- JavaCourseWork/src/main/java/com/shop_automation/controllers/GreetingController.java
+++ JavaCourseWork/src/main/java/com/shop_automation/controllers/GreetingController.java
@@ -1,11 +1,16 @@
 package com.shop_automation.controllers;
 
+import com.shop_automation.dto.ChargeDTO;
 import com.shop_automation.dto.SaleDTO;
 import com.shop_automation.dto.WarehouseDTO;
+import com.shop_automation.models.db.Charges;
+import com.shop_automation.models.db.ExpenseItems;
 import com.shop_automation.models.db.Sales;
 import com.shop_automation.models.db.Warehouses;
 import com.shop_automation.models.security.Role;
 import com.shop_automation.models.security.User;
+import com.shop_automation.services.db.ChargesService;
+import com.shop_automation.services.db.ExpenseItemsService;
 import com.shop_automation.services.db.SalesService;
 import com.shop_automation.services.db.WarehousesService;
 import com.shop_automation.services.security.UserService;
@@ -24,11 +29,15 @@ public class GreetingController {
     private final UserService userService;
     private final SalesService salesService;
     private final WarehousesService warehousesService;
+    private final ChargesService chargesService;
+    private final ExpenseItemsService expenseItemsService;
 
-    public GreetingController(UserService userService, SalesService salesService, WarehousesService warehousesService) {
+    public GreetingController(UserService userService, SalesService salesService, WarehousesService warehousesService, ChargesService chargesService, ExpenseItemsService expenseItemsService) {
         this.userService = userService;
         this.salesService = salesService;
         this.warehousesService = warehousesService;
+        this.chargesService = chargesService;
+        this.expenseItemsService = expenseItemsService;
     }
 
     @GetMapping
@@ -44,9 +53,9 @@ public class GreetingController {
                     sales.getWarehouses().getId()
             ));
         }
-        List<Warehouses> response = warehousesService.getWarehouses();
+        List<Warehouses> responseWarehouses = warehousesService.getWarehouses();
         List<WarehouseDTO> tempWarehouses = new ArrayList<>();
-        for (Warehouses warehouses : response) {
+        for (Warehouses warehouses : responseWarehouses) {
             tempWarehouses.add(new WarehouseDTO(
                     warehouses.getId(),
                     warehouses.getName(),
@@ -54,6 +63,19 @@ public class GreetingController {
                     warehouses.getAmount()
             ));
         }
+        List<Charges> responseCharges = chargesService.getCharges();
+        List<ChargeDTO> tempCharges = new ArrayList<>();
+        for (Charges charges : responseCharges) {
+            tempCharges.add(new ChargeDTO(
+                    charges.getId(),
+                    charges.getAmount(),
+                    charges.getCharge_date(),
+                    charges.getExpense_items().getId()
+            ));
+        }
+        List<ExpenseItems> responseItems = expenseItemsService.getItems();
+        model.put("items", responseItems);
+        model.put("charges", tempCharges);
         model.put("warehouses", tempWarehouses);
         model.put("sales", tempSales);
         return "greeting";
